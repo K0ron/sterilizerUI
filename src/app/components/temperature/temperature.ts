a420206500d63a623d841fea7c1d09b6c5970f23
K0ron <kevin.caron.91@gmail.com>
2026-02-02 16:39:51 +0100

    add progress bar and circle temperature

--- src/app/components/temperature/temperature.ts
+++ src/app/components/temperature/temperature.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-temperature',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './temperature.html',
   styleUrl: './temperature.scss',
 })
-export class Temperature {
+export class Temperature implements OnInit {
+
+  currentTemperature: number = 20;
+  targetTemperature: number = 105;
+
+  constructor(private cd : ChangeDetectorRef) {}
+
+ ngOnInit(): void {
+      const interval = setInterval(() => {
+      if (this.currentTemperature < this.targetTemperature) {
+        this.currentTemperature += 1; // +1°C à chaque tick
+        this.cd.detectChanges(); // forcer la détection des changements
+      } else {
+        clearInterval(interval); // on arrête quand target atteinte
+      }
+    }, 200);
+ }
+  
+
+  get progress(): number {
+    return Math.min((this.currentTemperature / this.targetTemperature) * 100, 100);
+
+  }
+
+  get progressColor(): string {
+    return this.currentTemperature < 50 ? '#00983f' : '#d42a17'; 
+  }
 
 }
